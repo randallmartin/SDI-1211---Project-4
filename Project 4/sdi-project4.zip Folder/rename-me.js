8e5426049f6a698bf0f28c6a8b776bbcbec76914
Randall Martin <Randallmartin@me.com>
2012-11-15 13:32:25 -0600

    Added toChangeTitleCase
    Added first part of toChangeTitleCase

--- Project 4/sdi-project4.zip Folder/rename-me.js
+++ Project 4/sdi-project4.zip Folder/rename-me.js
@@ -1,1 +1,18 @@
-alert("JavaScript works!");
+//Randall Martin
+//SDI 1211
+//Project 4
+
+var output = function(message){console.log (message);};
+
+var randallsLibrary = (function() {
+
+	var toChangeTitleCase = function (string) {
+		return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	};
+
+
+
+
+
+
+};
