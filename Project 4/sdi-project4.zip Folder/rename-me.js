d5c66b2d1950fd1085b234dd5c0dd5eabfc43e4f
Randall Martin <Randallmartin@me.com>
2012-11-15 13:37:38 -0600

    Added toValidateEmail & toReplaceCharacter

--- Project 4/sdi-project4.zip Folder/rename-me.js
+++ Project 4/sdi-project4.zip Folder/rename-me.js
@@ -9,7 +9,17 @@ var randallsLibrary = (function() {
 	var toChangeTitleCase = function (string) {
 		return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 	};
+	
+	var toValidateEmail = function (testEmail) {
+		var regexObj = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+		return (regexObj.test(testEmail));
+	};
 
+	var toReplaceCharacter = function (string,oldChar,newChar) {
+		var myString = string,
+		old = new RegExp(oldChar, "m");
+		return (myString.replace(old, newChar));
+	};
 
 
 
